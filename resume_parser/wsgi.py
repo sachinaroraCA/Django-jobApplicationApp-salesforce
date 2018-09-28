a253bece97bccbb83985878025dfdef9a54d6fb0
Sudhir singh <sudhir.singh@cloudanalogy.com>
2018-09-28 15:50:19 +0530

    "Intial commit 2"

--- resume_parser/wsgi.py
+++ resume_parser/wsgi.py
@@ -11,7 +11,4 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "resume_parser.settings")
-
-from whitenoise.django import DjangoWhiteNoise
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
