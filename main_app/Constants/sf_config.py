de03d4945623edc11a7f4201ecea1d42684f31dd
Sudhir singh <sudhir.singh@cloudanalogy.com>
2018-10-03 16:20:17 +0530

    "Changed credentials"

--- main_app/Constants/sf_config.py
+++ main_app/Constants/sf_config.py
@@ -1,11 +1,11 @@
 class ConnectionString:
 
     USERNAME = "analogforce@isvedition.org.partial"
-    PASSWORD = "Sanju@1122"
-    ACCESS_TOKEN = "00D290000000pY0!ARYAQAIT2XhXgWC3f98hfHrydbOXN6bNh2GPe8E0yweqJ6jSp6Olkc_k981Jy.5mNvOAknLaZPLRB1HgEkiQJFfD994Z4QpR"
+    PASSWORD = "sfdc112233"
+    ACCESS_TOKEN = "00D290000000pY0!ARYAQIU8xnTeT0elfakATzTENEJyQXD9dN7abWavIYJDlZtvYPMLJI1NosalRu_dqAZOEf9RVn136TeXUDV9uZxOTqdnsNJb"
     SF_URL = "cs19.salesforce.com"
     SF_URL_HTTP = "https://cs19.salesforce.com"
-    SECURITY_TOKEN = "KnYpFK0vF0zOvHa5c0zuXDLeK"
+    SECURITY_TOKEN = "JVE1LuWtQMkCkrmlJcuQtGvNM"
     CONSUMER_KEY = "3MVG9Vik22TUgUphWlC1Z3_7xqY1byIyizn2tCxsPzB1NjixeH8NcpGMci5Ge1O34PWokgwuPovlKQjJS6c2J"
     CLIENT_SECRET = "4906138348286418247"
     REFRESH_TOKEN = "5Aep8613hy0tHCYdhyiUsgtHzqfLdtsmdHtLp.mW0HtTtfVAyfYWjbddqKwNUL77sn3okdaw2iguA9BGi1MKOA9"
