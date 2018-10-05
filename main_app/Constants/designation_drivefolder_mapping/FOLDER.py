4ba660021d20e76e09c4edef3f09e9093389a9f2
Sudhir singh <sudhir.singh@cloudanalogy.com>
2018-10-05 17:09:20 +0530

    fix folders

--- main_app/Constants/designation_drivefolder_mapping/FOLDER.py
+++ main_app/Constants/designation_drivefolder_mapping/FOLDER.py
@@ -1,1 +1,4 @@
 folder_id = {}
+folder_id.update({'Developer': '1amaMKP4nO-NcRoN37MZlSg_hgQrNnG03'})
+folder_id.update({'Consultant': '1bpeYfZLZ4_fXfwJu9X1HCoSMObNCnGeI'})
+folder_id.update({'Sales': '174NaRApV4HHNtQIZFu3Xf2ZErsXxp-yd'})
