db4e3ed5ab774a44de4f3dd52616092c2a00fc3e
Sudhir singh <sudhir.singh@cloudanalogy.com>
2018-10-11 18:40:09 +0530

    Added xframe option function

--- main_app/views.py
+++ main_app/views.py
@@ -99,3 +99,10 @@ def upload_details(request):
         print("Exception:" + repr(ex))
         messages.error(request, 'Unable to process request.. try again later !!!')
     return HttpResponseRedirect('/')
+
+from django.http import HttpResponse
+from django.views.decorators.clickjacking import xframe_options_exempt
+
+@xframe_options_exempt
+def ok_to_load_in_a_frame(request):
+    return HttpResponse("This page is safe to load in a frame on any site.")
